40baac0958e7a8037bdb001e46b819b0cdc10443
yuqizhong2 <yuqizhong2@gmail.com>
2015-07-31 07:45:17 +0800

    123

--- IntoHeart/app/build/intermediates/classes/debug/ee3316/intoheart/DashboardFragment$$ViewInjector.java
+++ IntoHeart/app/build/intermediates/classes/debug/ee3316/intoheart/DashboardFragment$$ViewInjector.java
@@ -8,13 +8,13 @@ import butterknife.ButterKnife.Injector;
 public class DashboardFragment$$ViewInjector<T extends ee3316.intoheart.DashboardFragment> implements Injector<T> {
   @Override public void inject(final Finder finder, final T target, Object source) {
     View view;
-    view = finder.findRequiredView(source, 2131493013, "field 'sportsMan'");
-    target.sportsMan = finder.castView(view, 2131493013, "field 'sportsMan'");
-    view = finder.findRequiredView(source, 2131493014, "field 'heart'");
-    target.heart = finder.castView(view, 2131493014, "field 'heart'");
-    view = finder.findRequiredView(source, 2131492987, "field 'graph'");
-    target.graph = finder.castView(view, 2131492987, "field 'graph'");
-    view = finder.findRequiredView(source, 2131493015, "method 'showInstantView'");
+    view = finder.findRequiredView(source, 2131493000, "field 'sportsMan'");
+    target.sportsMan = finder.castView(view, 2131493000, "field 'sportsMan'");
+    view = finder.findRequiredView(source, 2131493002, "field 'heart'");
+    target.heart = finder.castView(view, 2131493002, "field 'heart'");
+    view = finder.findRequiredView(source, 2131493007, "field 'graph'");
+    target.graph = finder.castView(view, 2131493007, "field 'graph'");
+    view = finder.findRequiredView(source, 2131493003, "method 'showInstantView'");
     view.setOnClickListener(
       new butterknife.internal.DebouncingOnClickListener() {
         @Override public void doClick(
@@ -23,7 +23,7 @@ public class DashboardFragment$$ViewInjector<T extends ee3316.intoheart.Dashboar
           target.showInstantView(p0);
         }
       });
-    view = finder.findRequiredView(source, 2131493016, "method 'showDayView'");
+    view = finder.findRequiredView(source, 2131493004, "method 'showDayView'");
     view.setOnClickListener(
       new butterknife.internal.DebouncingOnClickListener() {
         @Override public void doClick(
@@ -32,7 +32,7 @@ public class DashboardFragment$$ViewInjector<T extends ee3316.intoheart.Dashboar
           target.showDayView(p0);
         }
       });
-    view = finder.findRequiredView(source, 2131493017, "method 'showWeekView'");
+    view = finder.findRequiredView(source, 2131493005, "method 'showWeekView'");
     view.setOnClickListener(
       new butterknife.internal.DebouncingOnClickListener() {
         @Override public void doClick(
@@ -41,7 +41,7 @@ public class DashboardFragment$$ViewInjector<T extends ee3316.intoheart.Dashboar
           target.showWeekView(p0);
         }
       });
-    view = finder.findRequiredView(source, 2131493018, "method 'showMonthView'");
+    view = finder.findRequiredView(source, 2131493006, "method 'showMonthView'");
     view.setOnClickListener(
       new butterknife.internal.DebouncingOnClickListener() {
         @Override public void doClick(
